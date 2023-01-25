ff101223087122c60592345e7e3379d29a603c5c
SU_VIN <pyotnqls25@naver.com>
2023-01-25 18:18:25 +0900

    page 연결
    Home, Me, Project, Contact page 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Me from "./pages/Me";
+import Project from "./pages/Project";
+import Contact from "./pages/Contact";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App"></div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/me" element={<Me />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
