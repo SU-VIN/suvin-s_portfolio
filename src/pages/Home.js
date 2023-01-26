9c87c5ce5179f0b9e318d1cd4afc5e67013a6153
SU_VIN <pyotnqls25@naver.com>
2023-01-26 15:55:23 +0900

    Home 제작1
    font- 빛고을광주체

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,18 @@
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <div className="Home">
+      <div className="Me_img">
+        <img src={process.env.PUBLIC_URL + `assets/LIM.png`} />
+      </div>
+      <div className="Me_content">
+        <h1>안녕하세요</h1>
+        <h1>개발자</h1>
+        <h1>임수빈입니다.</h1>
+        <hr />
+        <h5>방문해 주셔서 감사합니다.</h5>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
