fe385b3ff32434aeb8058599ce63428f80811711
SU_VIN <pyotnqls25@naver.com>
2023-01-27 01:10:29 +0900

    Home 깡통 제작 완료
    menu바는 contact페이지에서도 사용되기때문에 component로 제작하였음

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,5 +1,17 @@
+import { AiOutlineHome, AiOutlineFolder } from "react-icons/ai";
+import { BsPerson, BsTelephone } from "react-icons/bs";
+
 const MenuBar = () => {
-  return <div></div>;
+  return (
+    <div className="Menubar">
+      <div className="backgrondMenu">
+        <AiOutlineHome className="icon" />
+        <BsPerson className="icon" />
+        <AiOutlineFolder className="icon" />
+        <BsTelephone className="icon" />
+      </div>
+    </div>
+  );
 };
 
 export default MenuBar;
