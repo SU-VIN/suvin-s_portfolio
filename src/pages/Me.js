9394b1145d650330b1f7c74160b5e4f5b86857b0
SU_VIN <pyotnqls25@naver.com>
2023-02-01 15:48:26 +0900

    Me 제작3
    내용 채우기중

--- src/pages/Me.js
+++ src/pages/Me.js
@@ -20,9 +20,49 @@ const Me = () => {
         ) : (
           <></>
         )}
-        <h1>Me</h1>
+        <div className="aboutMe">
+          <h1>Me</h1>
+          <div className="hashTag">
+            <h5># 임수빈</h5>
+            <h5># 프로젝트</h5>
+            <h5># 수상경력</h5>
+          </div>
+        </div>
       </div>
-      <div className="bottom"></div>
+      <div className="bottom">
+        <h4>임수빈</h4>
+        <div className="suvin">
+          <img
+            className="pic"
+            src={process.env.PUBLIC_URL + `assets/SuvinPic.png`}
+          />
+          <div className="suvinList">
+            <p>2019. 02 | 경인고등학교 졸업</p>
+            <p>2019. 03 | 한성대학교 IT공과대학 입학</p>
+            <p>2020. 06 ~ 2022. 12 | 한성대학교 학술소모임 BUG 회원</p>
+            <p>2023. 02 | 한성대학교 졸업</p>
+          </div>
+        </div>
+
+        <h4>프로젝트</h4>
+        <div className="projectList">
+          <h3>
+            <p>HSU_On</p>한성대학교 메타버스 캠퍼스
+          </h3>
+          <h3>
+            <p>왕이되자</p>대부호 카드게임
+          </h3>
+          <h3>
+            <p>Hansung Vote</p>학생회 선거 웹페이지
+          </h3>
+          <h3>
+            <p>내 MBTI를 맞춰봐</p>서비스형 웹페이지
+          </h3>
+        </div>
+
+        <h4>수상경력</h4>
+        <div className="awards"></div>
+      </div>
     </div>
   );
 };
